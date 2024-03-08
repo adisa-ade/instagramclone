5ebcdb32dce5a9f17a4d776090a632eb9975f9bb
adisa <abubakradisa@gmail.com>
2024-03-08 15:35:44 +0100

    Added following and followers functionality

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,19 +1,32 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, reactive, onMounted, watch } from 'vue'
 import Container from "./Container.vue"
 import UserBar from "./UserBar.vue"
 import ImageGalleryVue from "./ImageGallery.vue";
 import {supabase} from "../supabase"
 import {useRoute} from "vue-router"
+import { useUserStore } from "@/stores/users";
+import {storeToRefs} from "pinia"
 
 const route = useRoute()
 const loading = ref(false)
 const user = ref(null)
 const {username} = route.params
 const posts = ref([])
+const userStore = useUserStore()
+const {user: loggedInUser} = storeToRefs(userStore)
+const isFollowing = ref(false)
+const userInfo = reactive({
+    posts: null,
+    followers: null,
+    following: null
+})
 
 const addNewPost = (post) => {
     posts.value.unshift(post)
+}
+const updateIsFollowing = (follow) => {
+    isFollowing.value = follow
 }
 const fetchData = async () => {
      loading.value = true
@@ -36,9 +49,47 @@ const fetchData = async () => {
     .select()
     .eq("owner_id", user.value.id)
     posts.value = postsData     
+    await fetchIsFollowing()
+    const followerCount = await fetchFollowersCount()
+    const followingCount = await fetchFollowingCount ()
+    userInfo.followers = followerCount
+    userInfo.following= followingCount
+    userInfo.posts = posts.value.length
     loading.value = false
 }
 
+const fetchFollowersCount = async () => {
+    const {count} = await supabase
+     .from("followers_following")
+     .select('*', {count: 'exact'})
+     .eq("following_id", user.value.id)
+     return count
+}
+
+const fetchFollowingCount = async () => {
+    const {count} = await supabase
+     .from("followers_following")
+     .select('*', {count: 'exact'})
+     .eq("follower_id", user.value.id)
+     return count
+}
+
+
+const fetchIsFollowing = async () =>{
+    if(loggedInUser.value && (loggedInUser.value.id !== user.value.id)){
+    const {data} = await supabase
+    .from("followers_following")
+    .select()
+    .eq("follower_id", loggedInUser.value.id) 
+    .eq("following_id", user.value.id)
+    .single()
+        if(data) isFollowing.value = true
+    }
+    
+}
+watch(loggedInUser, () => {
+    fetchData()
+})
 onMounted(() => {
     fetchData()
 })
@@ -49,12 +100,10 @@ onMounted(() => {
         <UserBar
         :key="$route.params.username"
         :user="user"
-        :userInfo="{
-            posts: 5,
-            followers: 50000,
-            following: 2,
-        }"
-        :addNewPost="addNewPost"
+        :userInfo="userInfo"
+        :addNewPost = "addNewPost"
+        :isFollowing = "isFollowing"
+        :updateIsFollowing = "updateIsFollowing"
         />            
         <ImageGalleryVue :posts="posts"
         />            
--- src/components/UserBar.vue
+++ src/components/UserBar.vue
@@ -1,19 +1,41 @@
 <script setup>
-const props = defineProps (['user', 'userInfo', 'addNewPost'])    
+const props = defineProps (['user', 'userInfo', 'addNewPost', 'isFollowing', 'updateIsFollowing'])    
 import UploadPhotoModal from "./UploadPhotoModal.vue"
 import {useRoute} from "vue-router"
 import { useUserStore } from "@/stores/users";
 import { storeToRefs } from "pinia";
+import {supabase} from "../supabase"
     const route = useRoute()    
     const userStore = useUserStore()
     const {user} = storeToRefs(userStore)        
     const {username: profileUserName} = route.params  
+    
+    const followUser = async () => {
+        props.updateIsFollowing(true)
+        await supabase.from("followers_following").insert({
+            follower_id: user.value.id,
+            following_id: props.user.id
+        })
+    }
+    const unFollowUser = async () => {
+        props.updateIsFollowing(false)
+        await supabase.from("followers_following")
+        .delete()
+        .eq("follower_id", user.value.id)
+        .eq("following_id", props.user.id)
+    }
 </script>
 <template>
     <div class="user-container" v-if="props.user">
     <div class="top-content">
         <a-typography-title :level="1">{{props.user.username.toUpperCase()}}</a-typography-title>  
-        <UploadPhotoModal v-if="user && profileUserName === user.username" :addNewPost="addNewPost"/>
+        <div v-if="user">
+            <UploadPhotoModal v-if="profileUserName === user.username" :addNewPost="addNewPost"/>
+            <div v-else>
+                <a-button v-if="!props.isFollowing" @click="followUser">Follow</a-button>
+                <a-button v-else @click="unFollowUser">Following</a-button>
+            </div>
+        </div>
     </div>
     <div class="bottom-content">
         <a-typography-title :level="5" style="margin:0; font-size:20px;">{{props.userInfo.posts}} Post</a-typography-title>
